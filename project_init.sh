86d67fc6f4a4b33d147e5fcf8c2f452fe8e46527
Maxime Mion <Maxime@Maximes-Mini.fritz.box>
2020-10-07 19:14:45 +0200

    added remove script

--- project_init.sh
+++ project_init.sh
@@ -4,7 +4,7 @@ function create(){
     cd
     #enter the path to the enclosing folder before the .env
     source .env
-    python3 $SCRIPTPATH $1 $2
+    python3 $SCRIPTPATHCREATE $1 $2
     cd $DESTPATH$1
     git init
     touch README.md
@@ -13,4 +13,14 @@ function create(){
     git remote add origin https://github.com/$USERNAME/$1.git
     git push -u origin master
     $IDE .
+    echo "successfully initiated $1 project!"
+}
+
+function remove(){
+    cd
+    #enter the path to the enclosing folder before the .env
+    source .env
+    python3 $SCRIPTPATHREMOVE $1
+    rm -rf $DESTPATH$1
+    echo "successfully deleted $1 project!"
 }
