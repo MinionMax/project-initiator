0e5a3603098261410659fed1cc491f39d4fc1004
MinionMax <maxime.mion@gmx.de>
2021-08-23 16:01:55 +0200

    🎉 initial commit

--- index.js
+++ index.js
@@ -2,5 +2,85 @@
 
 const { Octokit } = require("@octokit/rest");
 const dotenv = require("dotenv").config();
+const childProcess = require("child_process");
+const fs = require("fs");
+const path = require("path");
+const argv = require("minimist")(process.argv.slice(2));
+const TOKEN = process.env.TOKEN;
+const USERNAME = process.env.USERNAME;
+const PROJECTPATH = process.env.PROJECTPATH;
 
-console.log("Lmao");
+const octokit = new Octokit({
+    auth: TOKEN,
+    userAgent: USERNAME,
+    baseUrl: 'https://api.github.com',
+})
+
+function argumentValid(argv){
+
+    const argument = argv._[0];
+    const name = argv._[1]
+    const private = argv.p;
+    const help = argv.h;
+
+
+    switch(argument){
+        case "new":
+            newRepo(name, private);
+        break;
+        case "del":
+            deleteRepo(name);
+        break;
+        case "setup":
+            launchSetup();
+        break;
+        default:
+           if(!help) console.error("error in argument parsing, use project -h for available commands");
+           else if(help) provideHelp();
+        break;
+    }
+
+}
+argumentValid(argv);
+
+async function newRepo(name, private){
+    const project = path.join(PROJECTPATH, name);
+    console.log("🚚 connecting to github...");
+    await octokit.rest.repos.createForAuthenticatedUser({
+        name: name,
+        private: private
+    })
+    fs.mkdirSync(project);
+    fs.writeFile(path.join(project + "/README.md"), `# ${name}`, (err) => {
+        if (err) throw err;
+    })
+    fs.writeFile(path.join(project + ".gitignore"),"", (err) => {
+        if (err) throw err;
+    })
+    console.log("✨ initialising empty git repo...")
+    childProcess.exec("pwd", {
+            cwd: project
+        }, 
+        () => {
+            childProcess.exec("git init");
+            childProcess.exec("git add .");
+            childProcess.exec("git commit -m '🎉 initial commit'");
+            childProcess.exec(`git remote add origin https://github.com/${USERNAME}/${name}.git`);
+            childProcess.exec("git push");
+        }
+    )
+    console.log(`🎉 done! project ${name} is online...`)
+
+}
+
+function deleteRepo(name){
+    console.log("delete " + name)
+}
+
+function launchSetup(){
+    childProcess.fork("./setup.js");
+}
+
+function provideHelp(){
+    console.log("help")
+}
--- setup.js
+++ setup.js
@@ -1,26 +1,84 @@
 const fs = require("fs");
+const { exec } = require("child_process");
+const chalk = require("chalk");
 const readline = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-readline.question(`
-        please enter your github personal access token below...
+// acts as a global variable that will end up in .env file
+var nameString = ""
+var pathString = ""
 
-        for info on how to generate your token please visit this link:
-        https://docs.github.com/en/github/authenticating-to-github/keeping-your-account-and-data-secure/creating-a-personal-access-token
+function tryGetUsername(){
+    exec("git config user.name", (err, stdout, stderr) => {
+        if (err){
+            // console.log(err)
+            inputName();
+        } 
+        var name =stdout.replace(/\n$/, '')
+        nameString = `USERNAME="${name}"\n`
+        inputDestinationFolder();
+    })
+}
+tryGetUsername();
 
-        required permissions:
-        repo, delete_repo
+function inputDestinationFolder(){
+    clearScreen();
+    readline.question(chalk`
+            {yellow please enter the path to your coding projects folder...}
 
-    `,
-    (token) => {
-        fs.writeFile(".env", `TOKEN="${token}"`, (err) =>{
-            if(err) throw err;
-            console.log("setup complete!");
-            process.exit()
-        })
+            {gray you may also drag and drop the folder into the terminal}
+        `,
+        (path) => {
+            pathString = `PROJECTPATH="${path.trim()}"\n`
+            clearScreen();
+            inputToken();
+        }
+    )
+}
+
+function inputName(){
+    readline.question(chalk.yellow`
+
+            please enter your github username below...
+
+        `, 
+        (username) => {
+            nameString = `USERNAME="${username}"\n`
+            clearScreen()
+            inputDestinationFolder();
+        }
+    )
+}
+
+function inputToken(){
+    readline.question(chalk`
+        {yellow please enter your github personal access token below...}
+
+        {gray for info on how to generate your token please visit this link:
+        https://docs.github.com/en/github/authenticating-to-github/keeping-your-account-and-data-secure/creating-a-personal-access-token}
+
+        {red required permissions:
+        repo, delete_repo}
+
+        `,
+        (token) => {
+            var finalEnvString = nameString + pathString + `TOKEN="${token}"`
+            fs.writeFile(".env", finalEnvString, (err) =>{
+                if(err) throw err;
+                console.log(chalk.green("\nsetup complete!"));
+                process.exit()
+            })
+        }
+    )
+}
+
+function clearScreen(){
+    var lines = process.stdout.getWindowSize()[1];
+    for(var i = 0; i < lines; i++) {
+        console.log('\r\n');
     }
-)
+}
 
 
